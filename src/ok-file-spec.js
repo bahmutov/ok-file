c406aaaf790d5bd1b7b995b322c34d9728b91ef5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-05 17:11:06 -0400

    initial

---
+++ src/ok-file-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const okFile = require('.')
+
+describe('ok-file', () => {
+  it('write this test', () => {
+    console.assert(okFile, 'should export something')
+  })
+})
