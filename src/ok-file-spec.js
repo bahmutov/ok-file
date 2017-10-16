b742370780ca71e8bb6bc3320e1c54c823ec9a00
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-16 12:57:54 -0400

    fix(test): bin and test, close #1

--- src/ok-file-spec.js
+++ src/ok-file-spec.js
@@ -2,9 +2,19 @@
 
 /* eslint-env mocha */
 const okFile = require('.')
+const la = require('lazy-ass')
+const is = require('check-more-types')
 
 describe('ok-file', () => {
-  it('write this test', () => {
-    console.assert(okFile, 'should export something')
+  it('is a function', () => {
+    la(is.fn(okFile))
+  })
+
+  it('passes this file', () => {
+    la(okFile(__filename))
+  })
+
+  it('does not pass non-existent file', () => {
+    la(!okFile('does/not/exist'))
   })
 })
